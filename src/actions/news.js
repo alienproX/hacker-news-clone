40240fdf00d9b495b24b6ceb9f1db518c75a09fc
Cattla <cattla@me.com>
2016-04-07 03:57:46 +0800

    fix issue

--- src/actions/news.js
+++ src/actions/news.js
@@ -20,48 +20,53 @@ function fetchType(type){
         let idsArray = res.json()
         existNew = idsArray
         return idsArray
-    }),
+      }),
       receiveAction: constants.RECEIVE_NEWEST,
       nomoreAction: constants.NO_MORE_NEWEST
-  }
+    }
 
-  case 'show':
-  return {
+    case 'show':
+    return {
       existArray: existShow,
       apiPath: existShow ? existShow : fetch(`${API}showstories.json`)
       .then(function(res){
         let idsArray = res.json()
         existShow = idsArray
         return idsArray
-    }),
+      }),
       receiveAction: constants.RECEIVE_SHOW,
       nomoreAction: constants.NO_MORE_SHOW
-  }
+    }
 
-  case 'ask':
+    case 'ask':
 
-  return {
+    return {
       existArray: existAsk,
       apiPath: existAsk ? existAsk : fetch(`${API}askstories.json`)
       .then(function(res){
         let idsArray = res.json()
         existAsk = idsArray
         return idsArray
-    }),
+      }),
       receiveAction: constants.RECEIVE_ASK,
       nomoreAction: constants.NO_MORE_ASK
 
 
-  }
+    }
 
-  default:
-  return {
+    default:
+    return {
       existArray: existTop,
-      apiPath: existTop ? existTop : creatFetch(existTop, 'topstories'),
+      apiPath: existTop ? existTop : fetch(`${API}topstories.json`)
+      .then(function(res){
+        let idsArray = res.json()
+        existTop = idsArray
+        return idsArray
+      }),
       receiveAction: constants.RECEIVE_NEWS,
       nomoreAction: constants.NO_MORE_NEWS
+    }
   }
-}
 }
 
 function fetchNews(times, type) {
@@ -76,18 +81,18 @@ function fetchNews(times, type) {
 }
 
 function receiveNews(json, start, type) {
-    if(json.length > 0){
-      return {
-        type: fetchType(type).receiveAction,
-        json: json,
-        start: start
-      }
+  if(json.length > 0){
+    return {
+      type: fetchType(type).receiveAction,
+      json: json,
+      start: start
     }
-    else{
-      return {
-        type: fetchType(type).nomoreAction
-      }
+  }
+  else{
+    return {
+      type: fetchType(type).nomoreAction
     }
+  }
 }
 
 

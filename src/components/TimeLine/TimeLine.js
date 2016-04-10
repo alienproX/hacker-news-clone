5b805eb219e56bf6d3885e164d97d35eab4e2b52
Cattla <cattla@me.com>
2016-04-10 18:58:36 +0800

    scroll back

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -26,12 +26,7 @@ class TimeLine extends Component {
       }
     }
 
-    if(isEmpty(data.list) || window.createdNewsTime == data.created ){
-      loader = <Loader />
-      data.dispatch(data.fetchNews(0,data.type))
-    }
-    else{
-			window.createdNewsTime = data.created
+    if(!isEmpty(data.list) ){
       list = (
         data.list.map(function (item,index) {
           let comments = item.descendants > 0 ? `${item.descendants+(item.descendants == 1 ? ` comment`:` comments`)}`: `discuss`
